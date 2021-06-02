3a471484d38e250ca96ab6393e75932a615d90f3
Bruno <brunosalzani@hotmail.com>
2021-06-01 22:29:55 -0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,11 @@ git init
 git add -A
 git commit -m 'deploy'
 
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:bruno-salzani/blog-enfermagem-senac.git main:gh-pages
+git push -f git@github.com:bruno-salzani/blog-enfermagem-senac.git main
 
 cd -
