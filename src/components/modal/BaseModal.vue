41b6a0952fdb05adc5dfb9e3a6588b94effb3015
Bruno <brunosalzani@hotmail.com>
2021-08-17 12:20:45 -0300

    new post

--- src/components/modal/BaseModal.vue
+++ src/components/modal/BaseModal.vue
@@ -1,23 +1,10 @@
 <template>
-	<div
-		class="fixed inset-0 z-20 flex items-center justify-center pt-6 overflow-y-auto bg-white  sm:bg-black sm:bg-opacity-90 md:px-0 sm:pt-0"
-	>
-		<div
-			class="absolute flex items-center justify-center w-10 h-10 rounded-full  top-2 right-2 sm:top-5 sm:right-5 hover:bg-white hover:bg-opacity-10"
-		>
-			<button class="sm:text-white" type="button" @click="close">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="w-8 h-8"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-				</svg>
-			</button>
-		</div>
-		<div class="h-full max-w-2xl px-5 pt-6 pb-4 bg-white rounded-lg sm:shadow sm:h-auto">
+	<div class="relative flex justify-center">
+		<span
+			class="fixed inset-0 z-20 flex items-center justify-center pt-6 overflow-y-auto bg-white  sm:bg-black sm:bg-opacity-90 md:px-0 sm:pt-0"
+			@click="close"
+		></span>
+		<div class="fixed top-0 z-30 h-full max-w-2xl px-5 pt-6 pb-4 bg-white rounded-lg sm:top-32 sm:shadow sm:h-auto">
 			<div class="flex mb-4">
 				<div class="flex flex-col mx-auto mb-2">
 					<img
@@ -43,6 +30,20 @@
 				<div class="pb-3 ml-1 font-light text-gray-500 sm:pb-0">
 					<social-share :title="`${title}`" :id="id" :content="`${content}`"></social-share>
 				</div>
+			</div>
+			<div
+				class="absolute flex items-center justify-center w-10 h-10 bg-gray-500 rounded-full cursor-pointer  top-2 right-2 sm:top-3 sm:right-3 hover:bg-gray-700"
+				@click="close"
+			>
+				<button class="text-white focus:outline-none" type="button">
+					<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
+						<path
+							fill-rule="evenodd"
+							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</button>
 			</div>
 		</div>
 	</div>
